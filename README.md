805ee00e7fa9fe0a441bb8011055d8c8f55dfbbb
Patrick J. Rex <prexdesigns@gmail.com>
2024-02-08 18:28:43 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Python PDF 2 Audiobook Conversion Tool
+When I say simple, I mean sub 20 lines of python. 
+Feel free to grab and expand on this for fun. 
+
+### Todos
+1 Add play/pause 
+2 Add playback speed options
+3 ???? 
+4 Profit
---
+++ main.py
@@ -0,0 +1,17 @@
+import pyttsx3 
+import PyPDF2 
+import tkinter.filedialog
+
+book = tkinter.filedialog.askopenfilename()
+pdfreader = PyPDF2.PdfFileReader(book)
+pages = pdfreader.numPages
+
+for num in range(0, pages):
+    page = pdfreader.getPage(num)
+    text = page.extractText()
+    player = pyttsx3.init()
+    player.say(text)
+    player.runAndWait()
+
+   
+
